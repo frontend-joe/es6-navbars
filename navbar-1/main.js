8254d6ee281f254a4191131c3f27f8b4ce557adc
frontend-joe <frontendjoe@outlook.com>
2023-01-31 21:46:10 +0000

    initial commit

---
+++ navbar-1/main.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector("#nav");
+
+const onScroll = (event) => {
+  const scrollPosition = event.target.scrollingElement.scrollTop;
+  if (scrollPosition > 10) {
+    if (!nav.classList.contains("scrolled-down")) {
+      nav.classList.add("scrolled-down");
+    }
+  } else {
+    if (nav.classList.contains("scrolled-down")) {
+      nav.classList.remove("scrolled-down");
+    }
+  }
+};
+
+document.addEventListener("scroll", onScroll);
